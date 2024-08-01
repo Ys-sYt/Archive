222319cc2b95092e2f2d262c33b4792d29f4314e
YoshitoSHIMIZU <yoshiyoshi2039@gmail.com>
2024-08-01 23:21:45 +0300

    config

--- vite.config.js
+++ vite.config.js
@@ -6,7 +6,9 @@ const outDir = resolve(__dirname, 'dist')
 
 export default defineConfig({
     root,
-    base: '/Archive/',
+    base: process.env.GITHUB_PAGES  // この行を追加
+        ? "Archive"            // この行を追加
+        : "./", 
     publicDir: resolve(__dirname, 'public'), // publicディレクトリを明示的に指定
     build: {
         outDir,
